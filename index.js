69922cb912a0cf13383f4b0c2a402c9444f25ebe
blnche <peltierblanche@gmail.com>
2023-01-22 22:34:05 +0100

    stopped before detect collision

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+console.log(collisions);
+
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle ='white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const collisionsMap =[]
+for (let i = 0; i < collisions.length; i+= 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({position}) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -1035,
+    y: -280
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+            boundaries.push(
+                new Boundary({
+                    position: {
+                        x: j * Boundary.width + offset.x,
+                        y: i * Boundary.height + offset.y
+                    }
+                })
+            )
+    })
+})
+
+console.log(boundaries);
 
 const image = new Image()
 image.src = './Assets/Img/PelletIsle.png'
@@ -14,8 +55,57 @@ console.log(image)
 const playerImage = new Image()
 playerImage.src = './Assets/Img/playerDown.png'
 
-image.onload = () => {
-    c.drawImage(image, -1025, -280)
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw(){
+    c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position:{
+    x: offset.x,
+    y: offset.y,
+    },
+
+    image: image
+})
+
+const keys = {
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    }
+
+}
+const testBoundary = new Boundary ({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+const movables = [background, testBoundary]
+
+function animate () {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    // boundaries.forEach(Boundary => {
+    //     Boundary.draw()
+    // })
+    testBoundary.draw()
     c.drawImage(
         playerImage,
         0,
@@ -27,8 +117,62 @@ image.onload = () => {
         playerImage.width/4,
         playerImage.height
     )
-}
 
+    if (keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
+        movables.forEach((movable) => 
+            movable.position.y +=3
+        )}
+    else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+        movables.forEach((movable) => 
+            movable.position.x +=3
+        )}
+    else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
+        movables.forEach((movable) => 
+            movable.position.y -=3
+        )}
+    else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+        movables.forEach((movable) => 
+            movable.position.x -=3
+        )}
+}
+animate()
+
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
-    console.log(e.key)
+    switch (e.key) {
+        case 'ArrowUp' :
+            keys.ArrowUp.pressed = true
+            lastKey='ArrowUp'
+            break
+        case 'ArrowLeft' :
+            keys.ArrowLeft.pressed = true
+            lastKey='ArrowLeft'
+            break
+        case 'ArrowDown' :
+            keys.ArrowDown.pressed = true
+            lastKey='ArrowDown'
+            break
+        case 'ArrowRight' :
+            keys.ArrowRight.pressed = true
+            lastKey='ArrowRight'
+            break
+    }
+    console.log(keys)
+})
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'ArrowUp' :
+            keys.ArrowUp.pressed = false            
+            break
+        case 'ArrowLeft' :
+            keys.ArrowLeft.pressed = false            
+            break
+        case 'ArrowDown' :
+            keys.ArrowDown.pressed = false            
+            break
+        case 'ArrowRight' :
+            keys.ArrowRight.pressed = false            
+            break
+    }
+    console.log(keys)
 })
